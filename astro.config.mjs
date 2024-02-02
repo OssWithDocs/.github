6f3df5efa9c0a20ac1bca4a738a12cd4187ab014
NavyStack <137406386+Navystack@users.noreply.github.com>
2024-02-02 08:46:21 +0000

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://ossdocs.org",
   prefetch: {
-    prefetchAll: true,
+    prefetchAll: true
   },
+  integrations: [preact()]
 });
