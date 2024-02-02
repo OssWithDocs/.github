db764f89795ad758398e10d06fd138e11bc12940
NavyStack <137406386+Navystack@users.noreply.github.com>
2024-02-02 03:29:31 +0000

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://ossdocs.org',
+    prefetch: {
+        prefetchAll: true
+      },
+});
